bbb5a3f7c28ba7bf44a9cf52df572585f6e39e4d
Sergey <myemail@example.com>
2022-11-23 21:26:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Converter from "./components/converter";
 
 function App() {
+  const error = "Ошибка!!!";
+  const validNumber = 7;
+
+  const [form, setForm] = useState("");
+  const [valid, setValid] = useState(true);
+
+  const RegExp = /^#[0-9A-F]{6}$/i;
+  const validateValue = (value) =>
+    RegExp.test(value) ? setValid(value) : setValid(RegExp.test(value));
+
+  const handleChange = ({ target: { value } }) => {
+    value.length === validNumber && validateValue(value);
+    value.length <= validNumber && setForm(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={valid ? { background: `${valid}` } : { background: "#ff0000" }}
+    >
+      <Converter
+        form={form}
+        valid={valid}
+        handleChange={handleChange}
+        error={error}
+      />
     </div>
   );
 }
